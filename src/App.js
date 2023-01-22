31ab8b2bd92f17bd05396fb490def7ddf4e2604f
saravanan <s.k.saravanan94@gmail.com>
2023-01-22 16:19:17 +0530

    use reducer implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import UseState from './Component/UseState/UseState';
+import UseReducer from './Component/UseReducer/UseReducer';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
+      <UseReducer />
     </div>
   );
 }
